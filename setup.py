02548b9728f8cd1846ae3a0a7411f8006c7b6d28
Craig Loftus <craig@regulusweb.com>
2017-11-09 11:57:56 +0000

    Change to readme markdown

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 import os
 from setuptools import find_packages, setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
-    README = readme.read()
-
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
@@ -21,7 +18,6 @@ setup(
     include_package_data=True,
     license='MIT License',
     description='A few useful extensions for Wagtail',
-    long_description=README,
     url='https://regulusweb.com/',
     author='Craig Loftus',
     author_email='craig@regulusweb.com',
