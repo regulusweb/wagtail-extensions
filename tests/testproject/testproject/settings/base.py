e54094fd71097bd0d3fd485cd8cac9f93cd339e4
Craig Loftus <craig@regulusweb.com>
2017-11-21 14:23:57 +0000

    Update test settings to include app settings

--- tests/testproject/testproject/settings/base.py
+++ tests/testproject/testproject/settings/base.py
@@ -139,3 +139,5 @@ WAGTAIL_SITE_NAME = "testproject"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://example.com'
+
+from wagtail_extensions.app_settings import *
