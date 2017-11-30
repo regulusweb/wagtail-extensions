ea97ac193e3dd8e47ac01efb119ee7dd21609235
Craig Loftus <craig@regulusweb.com>
2017-11-30 16:31:33 +0000

    Add some simple humanize templatetags

--- wagtail_extensions/templatetags/wagtailextensions_tags.py
+++ wagtail_extensions/templatetags/wagtailextensions_tags.py
@@ -1,4 +1,8 @@
+from urllib.parse import urlsplit
+
 from django.template import Library
+from django.template.defaultfilters import stringfilter
+
 from wagtailgeowidget.app_settings import (
     GEO_WIDGET_ZOOM,
     GOOGLE_MAPS_V3_APIKEY,
@@ -21,3 +25,22 @@ def map_assets():
     return {
         'api_key': GOOGLE_MAPS_V3_APIKEY,
     }
+
+
+@register.filter
+@stringfilter
+def humanize_url(value):
+    return urlsplit(value)[1]
+
+
+
+@register.simple_tag
+def humanize_date_range(start, end):
+    day_fmt = '%d %b %Y'
+    if start == end:
+        return start.strftime(day_fmt)
+    elif (start.month, start.year) == (end.month, end.year):
+        start_fmt = '%d'
+    else:
+        start_fmt = '%d %b %Y'
+    return "{} - {}".format(start.strftime(start_fmt), end.strftime(day_fmt))
