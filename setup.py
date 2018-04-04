1cb9635aa45c7ce730e96ff3fe8bff22b142858f
Samir Shah <solaris.smoke@gmail.com>
2018-04-04 14:05:00 +0300

    Version bump

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ test_requires = [
 
 setup(
     name='regulus-wagtail-extensions',
-    version='2.0.1',
+    version='2.0.2',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
