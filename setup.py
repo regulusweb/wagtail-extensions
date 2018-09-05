1a41824dcf457f39cd73138d55b287afbcdeb12e
Samir Shah <samir@regulusweb.com>
2018-09-05 09:43:01 +0300

    Bump version and add Django 2.1 to test matrix.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ test_requires = [
 
 setup(
     name='regulus-wagtail-extensions',
-    version='2.2.0',
+    version='2.3.0',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
